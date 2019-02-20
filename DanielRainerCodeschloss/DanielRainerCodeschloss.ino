31f60b25896c499508019f5024282aafc6fe5e4a
Thomas Gatterer <t.gatterer@tsn.at>
2019-02-20 03:12:04 +0100

    Code für das Codeschloss jetzt so überarbeitet, dass er simpler ist und bei längerer Inaktivität und teilweise geöffneten Knöpfen der Alarm losgeht. Sond-Funktionalität erweitert

--- DanielRainerCodeschloss/DanielRainerCodeschloss.ino
+++ DanielRainerCodeschloss/DanielRainerCodeschloss.ino
@@ -1,9 +1,13 @@
+//author: Daniel Rainer
+//created: 19/02/2019
+
 /*
  * This program monitors buttons connected to the ATMega328p chip it is running on.
- * If a connection is interrupted it activates an alert in the form of a certain melody specified in alertTones,
- * except the connection is the one specified in the openingSequence.
- * If all buttons were opened in the right sequence feedback is given in the form of a certain melody specified in unlockedTones.
+ * If a connection is interrupted it activates an alert,
+ * except the connection is the one specified in the sequenceOfButtons.
+ * If all buttons were opened in the right sequence feedback is auditive given.
  * Upon closing the buttons again the program will reset.
+ * Taking too long to unlock the buttons also causes the alert to be triggered.
  * 
  * How to connect everything:
  * 
@@ -20,156 +24,177 @@
  * S: speaker/sound (connect the speaker to GND)
  */
 
-const int numOfButtons = 3; //the number of buttons used in the sequence
-const int buttonPins[numOfButtons] = {1,5,7}; //the Arduino pins the buttons are connected to. For pin positions on the ATMega328p refer to http://www.hobbytronics.co.uk/image/data/tutorial/arduino-hardcore/atmega328-arduino-pinout.jpg or some other source.
-const int openingSequence[numOfButtons] = {5,1,7}; //use the pin numbers of buttonPins in any order
-int positionInSequence = 0; //tracks up to which position in the openingSequence buttons may be open
-const int soundPin = 8; //sound signal output on this pin
+//the pin the speaker is connected to
+const byte soundPin = 8;
+//the number of buttons used
+const byte numberOfButtons = 3;
+//the pins the buttons are attached to, in the order the must be opened for success
+//length of array must be equal to number of buttons
+const byte sequenceOfButtons[] = {5, 1, 7};
+//tracks the state of each button, true if open, false if closed
+boolean openButtons[numberOfButtons];
+//keeps track of how many buttons were already opened correctly
+byte positionInSequence = 0;
+//updates when a button is opened or closed, stores the milliseconds passed since runtime started
+long timeOfLastAction;
+//whe the buttons are partially open the alert will play after so many milliseconds of inactivity
+const int timeUntilTimeout = 3000;
+//prevents the alert from being triggered for this many milliseconds after a button was closed with others remaining open
+const int timeBeforeResetWhenButtonGetsClosed = 3000;
+//true if all buttons were opened in correct sequence, otherwise false
+boolean unlocked = false;
 
-//settings for the sound to be played upon successfully unlocking all buttons in the right order
-const int numOfUnlockedTones = 3; //length of the following array (unlockedTones)
-const int unlockedTones[numOfUnlockedTones] = {523, 659, 784}; //a sequence of tones of equal length, tones defined in Hertz.
-const int unlockedSoundRepetitions = 5; //Defines how often the above sequence (unlockedTones) should be played.
-const int unlockedTonesLengthMS = 300; //Defines the amount of time in milliseconds each tone of the sequence should be played.
+//settings for the alert sound
+//the number of tones used
+const byte numberOfAlertTones = 4;
+//the frequency of the tones in Hertz, length must be equal to numberOfAlertTones
+const int alertTones[] = {400, 300, 200, 100};
+//the duration of the tones in milliseconds, length must be equal to numberOfAlertTones
+const int alertTonesDurations[] = {200, 200, 200, 200};
 
-//settings for the sound to be played when a mistake is made in opening the buttons
-const int numOfAlertTones = 3; //length of the following array (alertTones)
-const int alertTones[numOfAlertTones] = {247, 196, 165}; //a sequence of tones of equal length, tones defined in Hertz.
-const int alertSoundRepetitions = 3; //Defines how often the above sequence (alertTones) should be played.
-const int alertTonesLengthMS = 700; //Defines the amount of time in milliseconds each tone of the sequence should be played.
+//settings for the success sound
+//the number of tones used
+const byte numberOfSuccessTones = 4;
+//the frequency of the tones in Hertz, length must be equal to numberOfSuccessTones
+const int successTones[] = {600, 700, 800, 900};
+//the duration of the tones in milliseconds, length must be equal to numberOfAlertTones
+const int successTonesDurations[] = {100, 100, 100, 100};
 
-//Sets the correct pinModes. Resets the program when called later.
 void setup() {
-  for(int i = 0; i < numOfButtons; i++){
-    pinMode(buttonPins[i], INPUT_PULLUP);
+  //sets each button pin to INPUT_PULLUP, meaning that digitaRead will return false if it is connected to GND, otherwise true
+  for(byte i = 0; i < numberOfButtons; i++){
+    pinMode(sequenceOfButtons[i], INPUT_PULLUP);
   }
+  //sets the soundPin to OUTPUT
   pinMode(soundPin, OUTPUT);
 }
 
-//main loop of the program. function restarts when it finished
-//causes the buttons states to be checked repeatedly
 void loop() {
- checkButtonStates();
- delay(100);
+  //updates the array which keeps track for each button if it is open or closed
+  updateButtonStates();
+  //checks for invalid behavious and triggers the alert upon detection of such
+  if(!checkIfButtonStatesValid() || checkForTimeout()){
+    alert();
+  }
+  //the previously called checkIfButtonStatesValid function sets unlocked to true if all buttons were opened correctly
+  if(unlocked){
+    success();
+  }
+  //delay in milliseconds to limit resource usage
+  delay(100);
 }
 
-//checks the state of each button
-void checkButtonStates(){
-  //loops through all buttons
-  for(int i = 0; i < numOfButtons; i++){
-    if(buttonOpen(buttonPins[i])){
-      //this button is open
-      if(!mayBeOpen(buttonPins[i])){
-        //wrong button opened
-        alert();
-      }else if (positionInSequence == numOfButtons){
-        //all buttons opened
-        unlocked();
-      }
+//updates the array which keeps track for each button if it is open or closed
+void updateButtonStates(){
+  //each iteration checks the state of one button
+  for(byte i = 0; i < numberOfButtons; i++){
+    boolean updatedValue = digitalRead(sequenceOfButtons[i]);
+    //if change is detected timeOfLastAction is set to the current time
+    if(openButtons[i] != updatedValue){
+      timeOfLastAction = millis();
     }
+    //value in the array gets updated
+    openButtons[i] = updatedValue;
   }
 }
 
-//tell if a button is open (connection interrupted)
-boolean buttonOpen(int pinNum){
-  boolean isOpen = digitalRead(pinNum); //true if open (no connection to GND, otherwise false
-  return isOpen;
-}
-
-//checks if a certain button may currently be open
-boolean mayBeOpen(int pinNum){
-  //positionInSequence keeps track where in the sequence the program is, meaning which buttons have been opened and which one may be opened next.
-  for(int i = 0; i <= positionInSequence; i++){
-    if (pinNum == openingSequence[i]){
-      //This button may be open.
-      if(i == positionInSequence){
-        //This button may be open and is the next button of the sequence, therefor positionInSequence gets incremented.
-        positionInSequence++;
-      }
-      //The button may be open, therefore true is retuned.
-      return true;
+//returns true if all buttons are in a valid state, otherwise returns false
+//additionally it sets unlocked to true if all buttons were opened correctly
+boolean checkIfButtonStatesValid(){
+  //if a previously opened button was closed before all buttons were opened
+  //the sequence resets after the delay in milliseconds defined by timeBeforeResetWhenButtonGetsClosed
+  for(byte i = 0; i < positionInSequence; i++){
+    if(!openButtons[i]){
+      delay(timeBeforeResetWhenButtonGetsClosed);
+      reset();
+      break;
     }
   }
-  //This is only reached when the button may not be open. (It comes later in the sequence.)
+  //if a button was opened before all of the preceeding ones in the sequence the buttons states are not valid
+  for(byte i = positionInSequence + 1; i < numberOfButtons; i++){
+    if(openButtons[i]){
+      return false;
+    }
+  }
+  //if the next button in the sequence was opened the position in the sequence increments
+  if(openButtons[positionInSequence]){
+    positionInSequence++;
+    //if the following is true all buttons were opened and unlocked can be set to true
+    if(positionInSequence == numberOfButtons){
+     unlocked = true;
+    }
+  }
+  return true;
+}
+
+//prolonged inactivity leads to a reset of the sequence
+//returns true if the time limit for opening the next button is exceeded, otherwise returns true
+boolean checkForTimeout(){
+  if(millis() > timeOfLastAction + timeUntilTimeout && !checkIfAllButtonsClosed()){
+    return true;
+  }
   return false;
 }
 
-//a wrong button was opened
+//this function is called after an illegal action was committed
+//it plays the alert sound and resets the sequence once all buttons are closed
 void alert(){
-  //plays a melody defined in the alert sound settings
-  alertSound();
-  //When all buttons are closed again the program gets reset.
+  playSound(numberOfAlertTones, alertTones, alertTonesDurations);
   while(true){
-    //Loop gets interrupted when all buttons are closed again (connected to the right pins and GND), causing a reset of the program.
-    if(allButtonsClosed()){
+    if(checkIfAllButtonsClosed()){
+      reset();
       break;
     }
-    delay(100);
   }
-  reset();
 }
 
-//Causes the melody defined in the alert sound settings to be played.
-void alertSound(){
-  //Plays the melody alertSoundRepetitions times.
-  for(int i = 0; i < alertSoundRepetitions; i++){
-    //Loops through all the tones of the alertTones array.
-    for(int j = 0; j < numOfAlertTones; j++){
-      //Starts playing a sound on the pin given to the function as the first parameter by switching the pin on and off again n times per second, n being the second parameter and the frequency in Hertz of the tone.
-      //Tone keeps playing until another tone is played or noTone() is called.
-      tone(soundPin, alertTones[j]);
-      //Defines how long the tone is to be played.
-      delay(alertTonesLengthMS);
-    }
-  }
-  //Stops the tone currently played.
-  noTone(soundPin);
-}
-
-//Returns true if all button pins are connected to GND, otherwise false.
-boolean allButtonsClosed(){
-  for(int i = 0; i < numOfButtons; i++){
-    if(digitalRead(buttonPins[i]) == HIGH){
+//returns true if all buttons are closed, otherwise returns false
+boolean checkIfAllButtonsClosed(){
+  for(byte i = 0; i < numberOfButtons; i++){
+    //if this button is open not all buttons are closed, hence false is returned
+    if(digitalRead(sequenceOfButtons[i])){
       return false;
     }
   }
   return true;
 }
 
-//All buttons were opened in correct order.
-//When the melody finished playing and all buttons are closed (buttons pins connected to GND) again the program gets reset.
-void unlocked(){
-  //Plays the melody defined in the unlocked sound settings
-  unlockedSound();
+//this function is called after all buttons were opened in the correct order
+//the function plays the alert sound and resets the sequence once all buttons are closed
+void success(){
+  playSound(numberOfSuccessTones, successTones, successTonesDurations);
   while(true){
-    //Loop gets interrupted when all buttons are closed again (connected to the right pins and GND), causing a reset of the program.
-    if(allButtonsClosed()){
+    if(checkIfAllButtonsClosed()){
+      reset();
       break;
     }
-    delay(100);
   }
-  reset();
 }
 
-//Plays the melody defined in the unlocked sound settings.
-void unlockedSound(){
-  //Plays the melody unlockedSoundRepetitions times.
-  for(int i = 0; i < unlockedSoundRepetitions; i++){
-    //Loops through all the tones of the unlockedTones array.
-    for(int j = 0; j < numOfUnlockedTones; j++){
-      //Starts playing a sound on the pin given to the function as the first parameter by switching the pin on and off again n times per second, n being the second parameter and the frequency in Hertz of the tone.
-      //Tone keeps playing until another tone is played or noTone() is called.
-      tone(soundPin, unlockedTones[j]);
-      //Defines how long the tone is to be played.
-      delay(unlockedTonesLengthMS);
-    }
+//used for playing sound
+//takes the number of tones, 
+//followed by an array containing the frequency of each individual tone in Hertz,
+//followed by an array containing the duration of each individual tone in milliseconds
+void playSound(byte numberOfTones, int tones[], int durations[]){
+  for(byte i = 0; i < numberOfTones; i++){
+    //starts to put out a signal on the pin with the number indicated by soundPin
+    //with the frequency defined by the second argument
+    tone(soundPin, tones[i]);
+    //the tone is played for this many milliseconds
+    delay(durations[i]);
+    //stops the tone
+    noTone(soundPin);
   }
-  //Stops the tone currently played.
-  noTone(soundPin);
 }
 
-//Resets the program.
+//called for resetting the program
+//resets all relevant variables
+//loop can continue after this function returned
 void reset(){
   positionInSequence = 0;
-  setup();
+  unlocked = false;
+  //timeOfLastAction = millis();
+  for(byte i = 0; i < numberOfButtons; i++){
+    openButtons[i] = false;
+  }
 }
